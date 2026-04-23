f0a10e565009df79c08b0d950803ebf24f8d719f
Fryzux <nolik534@mail.ru>
2026-04-23 14:41:27 +0000

    fix: resolve migration conflicts after frontend-osip merge

---
+++ studio/migrations/0004_merge_hall_description.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('studio', '0004_hall_description'),
+        ('studio', '0004_seed_default_halls'),
+    ]
+
+    operations = [
+    ]
